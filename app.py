0163a224ef7b040892a750d0d684f8653ecce7ae
GT <55749965+gtiralongo@users.noreply.github.com>
2024-08-01 13:44:15 -0300

    0.02.07

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from playwright.sync_api import sync_playwright
 
 # Simulación de credenciales para el ejemplo
 credentials = st.secrets["auth"]
@@ -6,6 +7,23 @@ credentials = st.secrets["auth"]
 # Gestión de estado de la aplicación
 if 'authenticated' not in st.session_state:
     st.session_state.authenticated = False
+
+# Función para obtener el estado de cuenta utilizando Playwright
+def obtener_estado_cuenta(username, password):
+    try:
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True)
+            page = browser.new_page()
+            page.goto("https://acceso.web.aysa.com.ar/saml2/idp/sso/acceso.web.aysa.com.ar")
+            page.fill('#username_field_id', username)  # Ajustar ID según el campo real
+            page.fill('#password_field_id', password)  # Ajustar ID según el campo real
+            page.click('button[type="submit"]')
+            page.wait_for_load_state('networkidle')  # Esperar a que la página se cargue completamente
+            estado_cuenta = page.inner_text('#elemento_estado_cuenta')  # Ajustar selector según sea necesario
+            browser.close()
+            return estado_cuenta
+    except Exception as e:
+        return f"Error al obtener el estado de la cuenta: {e}"
 
 # Página de Inicio de Sesión
 def login_page():
@@ -26,11 +44,15 @@ def admin_page():
     st.write("¡Bienvenido, administrador!")
     
     # Ejemplo de opciones en el panel de administración
-
     tab1, tab2, tab3 = st.tabs(["Aysa", "Metrogas", "Edenor"])
 
     with tab1:
         st.write("Aysa")
+        if st.button("Consultar Estado de Cuenta Aysa"):
+            username = st.secrets["account"]["username"]
+            password = st.secrets["account"]["password"]
+            estado_cuenta = obtener_estado_cuenta(username, password)
+            st.write(estado_cuenta)
     
     with tab2:
         st.write("Metrogas")
